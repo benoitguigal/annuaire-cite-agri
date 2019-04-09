c8bb3a27a20540e97c6a1b59a3c9ff63d195cc2a
benoitguigal <benoit.guigal@gmail.com>
2019-04-09 10:09:03 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+
+var mymap = L.map('map').setView([43.2804, 5.3806], 13);
+
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYmVub2l0Z3VpZ2FsIiwiYSI6ImNqdThob2Y5MDA5Zmg0NG4wMHg4Ynl0MHgifQ.CpODeh1akjBOEIXye5JXcQ', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.light',
+    accessToken: 'pk.eyJ1IjoiYmVub2l0Z3VpZ2FsIiwiYSI6ImNqdThob2Y5MDA5Zmg0NG4wMHg4Ynl0MHgifQ.CpODeh1akjBOEIXye5JXcQ'
+}).addTo(mymap);
+
+
+
+var marker = L.marker([43.302833, 5.386664]).addTo(mymap);
+
+marker.bindPopup("Les Paniers Marseillais")
+
+Papa.parse("/data/annuaire.geocoded.csv", {
+    download: true,
+    header: true,
+    complete: function(results) {
+        results.data.forEach(function(record) {
+            var marker = L.marker([record.latitude, record.longitude]).addTo(mymap)
+            marker.bindPopup(record.Nom)
+        })
+    }
+})
